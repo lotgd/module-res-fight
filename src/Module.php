24e49e90c3b8e5544149b1513b6bdc1b3262d9f3
Vassyli <basilius.sauter@gmail.com>
2017-11-18 14:59:18 +0100

    Initial Commit; Adds a first running module.
    This module adds some basic stuff around fights, such as turn regeneration upon new day.
    Dead user will get a turn penalty upon new day, but will recover all of their health.
    Tests are running well so far.

---
+++ src/Module.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+namespace LotGD\Module\Res\Fight;
+
+use Doctrine\Common\Util\Debug;
+use LotGD\Core\Game;
+use LotGD\Core\Events\EventContext;
+use LotGD\Core\Module as ModuleInterface;
+use LotGD\Core\Models\Module as ModuleModel;
+use LotGD\Module\NewDay\Module as NewDayModule;
+
+class Module implements ModuleInterface {
+    const ModuleIdentifier = "lotgd/module-res-fight";
+
+    const CharacterPropertyBattleState = self::ModuleIdentifier . "/battleState";
+    const CharacterPropertyTurns = self::ModuleIdentifier . "/turns";
+
+    public static function handleEvent(Game $g, EventContext $context): EventContext
+    {
+        $event = $context->getEvent();
+
+        if ($event === NewDayModule::HookAfterNewDay) {
+            $turns = 20;
+            $viewpoint = $context->getDataField("viewpoint");
+            print("It is a new day. Character health is ");
+            print($g->getCharacter()->getHealth());
+            print("\n");
+
+            if ($g->getCharacter()->isAlive() == false) {
+                $turns-=5;
+                $viewpoint->addDescriptionParagraph(
+                    sprintf("You are back from the dead. Since you died yesterday, you can only fight for %s rounds today.", $turns)
+                );
+            } else {
+                $viewpoint->addDescriptionParagraph(
+                    sprintf("You feel energized! Today, you can fight for %s rounds.", $turns)
+                );
+            }
+
+            $g->getCharacter()->setProperty(self::CharacterPropertyTurns, $turns);
+            $g->getCharacter()->setHealth($g->getCharacter()->getMaxHealth());
+        }
+
+        return $context;
+    }
+    
+    public static function onRegister(Game $g, ModuleModel $module) { }
+    public static function onUnregister(Game $g, ModuleModel $module) { }
+}
---
+++ tests/ModuleTest.php
@@ -0,0 +1,79 @@
+<?php
+declare(strict_types=1);
+
+namespace LotGD\Module\Res\Fight\Tests;
+
+use Doctrine\Common\Util\Debug;
+use LotGD\Core\Events\EventContext;
+use LotGD\Core\Events\EventContextData;
+use LotGD\Core\Models\Scene;
+use Monolog\Logger;
+use Monolog\Handler\NullHandler;
+
+use LotGD\Core\Configuration;
+use LotGD\Core\Game;
+use LotGD\Core\Models\Character;
+use LotGD\Core\Models\Module as ModuleModel;
+
+use LotGD\Module\Res\Fight\Module;
+use LotGD\Module\Res\Fight\Fight;
+
+class ModuleTest extends ModuleTestCase
+{
+    const Library = 'lotgd/module-res-fight';
+
+    protected function getDataSet(): \PHPUnit_Extensions_Database_DataSet_YamlDataSet
+    {
+        return new \PHPUnit_Extensions_Database_DataSet_YamlDataSet(implode(DIRECTORY_SEPARATOR, [__DIR__, 'datasets', 'module.yml']));
+    }
+
+    public function testHandleUnknownEvent()
+    {
+        // Always good to test a non-existing event just to make sure nothing happens :).
+        $context = new EventContext(
+            "e/lotgd/tests/unknown-event",
+            "none",
+            EventContextData::create([])
+        );
+
+        Module::handleEvent($this->g, $context);
+    }
+
+    public function testNewDayEventForAliveCharacter()
+    {
+        /** @var Game $game */
+        $game = $this->g;
+        /** @var Character $character */
+        $character = $this->getEntityManager()->getRepository(Character::class)->findById(1)[0];
+        $game->setCharacter($character);
+        $v = $game->getViewpoint();
+
+        // Assert new day happened
+        $this->assertSame("It is a new day!", $v->getTitle());
+
+        // Assert that our new day inserts work
+        $descriptions = explode("\n\n", $v->getDescription());
+        $this->assertContains("You feel energized! Today, you can fight for 20 rounds.", $descriptions);
+        $this->assertSame($character->getMaxHealth(), $character->getHealth());
+    }
+
+    public function testNewDayEventFoDeadCharacter()
+    {
+        /** @var Game $game */
+        $game = $this->g;
+        /** @var Character $character */
+        $character = $this->getEntityManager()->getRepository(Character::class)->findById(4)[0];
+        $game->setCharacter($character);
+        $this->assertSame(0, $character->getHealth());
+
+        $v = $game->getViewpoint();
+
+        // Assert new day happened
+        $this->assertSame("It is a new day!", $v->getTitle());
+
+        // Assert that our new day inserts work
+        $descriptions = explode("\n\n", $v->getDescription());
+        $this->assertContains("You are back from the dead. Since you died yesterday, you can only fight for 15 rounds today.", $descriptions);
+        $this->assertSame($character->getMaxHealth(), $character->getHealth());
+    }
+}
